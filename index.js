b30ee45feafedfe1ef043a5ad13bfffdb4c217b8
wintowasted <84101286+wintowasted@users.noreply.github.com>
2022-08-10 13:01:11 +0300

    step1 is completed
    All the necessary metrics are scraped from the list of urls which is provided in 'url.xlsx'

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { off } = require('process')
 const puppeteer = require('puppeteer')
-const url = 'https://www.markastok.com/tommy-hilfiger-espadril-bayan-ayakkabi-fw0fw06497-ybi-beyaz'
+const reader = require('xlsx')
+const root_url = 'https://markastok.com'
+
+const file = reader.readFile('urls.xlsx')
+let data = []
+const sheets = file.SheetNames
+const temp = reader.utils.sheet_to_json(file.Sheets[sheets[0]])
+temp.forEach(x => data.push(x))
+
 const productNames = []
 const productPrices = []
 const productCodes = []
@@ -8,18 +16,51 @@ const productAvailabilities = []
 const productOffers = []
 const productSalePrices = []
 
-async function start(){
-    const browser = await puppeteer.launch()
+
+async function step1(){
+    const browser = await puppeteer.launch({
+        headless: true,
+    })
     const page = await browser.newPage()
-    await page.goto(url)
+
+    await page.setRequestInterception(true)
+    page.on('request', (request) => {
+    if (request.resourceType() === 'image') request.abort()
+    else request.continue()
+})
+    for(let i=0;i<30;i++){
+    await page.goto(root_url + data[i].url,{ waitUntil: 'domcontentloaded' })
     
+    
+    const el = await page.$(".product-name") || ""
+   
+    const stock = await page.$(".fl.productFunction2") || ""
+    /*const name = await page.evaluate(() => {
+        return document.querySelector(".product-name")
+    })*/
+
+    /*const stock = await page.evaluate(() => {
+        return document.querySelector(".fl.productFunction2")
+    })*/
+
+    if(el != "" && stock == "")
+        await scrapeMetrics(page)
+    }
+
+    console.log(productNames,productPrices,productAvailabilities,productOffers,productSalePrices)
+    await browser.close()
+}
+
+
+async function scrapeMetrics(page){
     // Product Name
     const productName = await page.evaluate(()=>{
         const fullText = document.querySelector(".product-name")
         return fullText.textContent.slice(fullText.firstElementChild.textContent.length,).replace(/^\s\n|\n$/g,"")
     })
     productNames.push(productName)
-
+    
+    
     // Product Price
     const productPrice = await page.evaluate(() =>{
         return document.querySelector(".product-price").textContent.trim()
@@ -46,7 +87,13 @@ async function start(){
 
     // Product Offer
     const productOffer = await page.evaluate(() => {
-        return document.querySelector('.campaign-symbol').innerText.match(/\d+/)[0]
+        const offer = document.querySelector('.campaign-symbol')
+        if(offer == null){
+            return 0;
+        }
+        else{
+            return offer.innerText.match(/\d+/)[0]
+        }
     })
     productOffers.push(`%${productOffer}`)
 
@@ -54,10 +101,6 @@ async function start(){
     const productSalePrice = parseFloat(productPrice.replace(",",".")) - ((parseFloat(productPrice.replace(",",".")) * productOffer) / 100)
     strSalePrice = `${productSalePrice.toFixed(2).replace(".",",")}`
     productSalePrices.push(strSalePrice)
-
-    console.log(productNames,productPrices,productAvailabilities,productOffers,productSalePrices)
-    await browser.close()
 }
 
-
-start()
+step1()
